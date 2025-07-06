e8768d3141dd5ba4e4d0253704a460728629992e
Josh <karta.josh.natin@gmail.com>
2025-07-06 18:48:56 -0300

    feat: update layout structure for improved readability and styling

--- app/page.tsx
+++ app/page.tsx
@@ -56,7 +56,8 @@ export default function Page() {
   };
 
   return (
-    <main className="mx-auto flex h-dvh w-full items-center justify-center">
+    <main className="mx-auto flex h-dvh w-full flex-col items-center justify-center">
+      <h2 className="mb-10 text-4xl font-bold">Thinkfast</h2>
       {username === null ? (
         <Card>
           <CardContent>
