cb9a0228e0e4df3eeb126193e9e4ec0aba548ec6
Josh <karta.josh.natin@gmail.com>
2025-08-09 14:25:12 -0300

    feat: add DotPattern and Highlighter components for enhanced UI effects and animations

--- app/page.tsx
+++ app/page.tsx
@@ -21,6 +21,9 @@ import {
 import CreatePlaygroundForm from "@/components/forms/CreatePlaygroundForm";
 import JoinPlaygroundForm from "@/components/forms/JoinPlaygroundForm";
 import { Edit2 } from "lucide-react";
+import { Highlighter } from "@/components/magicui/highlighter";
+import { DotPattern } from "@/components/magicui/dot-pattern";
+import { cn } from "@/lib/utils";
 
 export default function Page() {
   const { username, setUsername } = useUsername();
@@ -56,113 +59,125 @@ export default function Page() {
 
   return (
     <main className="mx-auto flex h-dvh w-full flex-col items-center justify-center">
-      <h2 className="mb-4 text-4xl font-bold">Thinkfast</h2>
-      {username === null ? (
-        <Card>
-          <CardContent>
-            <Input
-              value={input}
-              onChange={(e) => setInput(e.target.value)}
-              placeholder="Enter username"
-              onKeyDown={(e) => e.key === "Enter" && handleSubmit()}
-            />
-            <Button onClick={handleSubmit} className="mt-2 w-full">
-              Save
-            </Button>
-          </CardContent>
-        </Card>
-      ) : (
-        <Card className="w-[300px] gap-4 md:w-[500px]">
-          <CardHeader>
-            {isChangingUsername ? (
-              <div className="space-y-4">
-                <CardTitle className="text-center font-normal">
-                  Change Username
-                </CardTitle>
-                <div className="space-y-2">
-                  <Input
-                    value={newUsername}
-                    onChange={(e) => setNewUsername(e.target.value)}
-                    placeholder="Enter new username"
-                    onKeyDown={(e) =>
-                      e.key === "Enter" && handleSaveNewUsername()
-                    }
-                  />
-                  <div className="flex gap-2">
-                    <Button onClick={handleSaveNewUsername} className="flex-1">
-                      Save
-                    </Button>
-                    <Button
-                      variant="outline"
-                      onClick={handleCancelChangeUsername}
-                      className="flex-1"
-                    >
-                      Cancel
-                    </Button>
+      <DotPattern
+        className={cn(
+          "z-10 [mask-image:radial-gradient(600px_circle_at_center,white,transparent)]",
+        )}
+      />
+      <div className="z-30">
+        <h2 className="mb-4 text-center text-4xl font-bold">
+          <Highlighter action="highlight">Thinkfast</Highlighter>
+        </h2>
+        {username === null ? (
+          <Card>
+            <CardContent>
+              <Input
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+                placeholder="Enter username"
+                onKeyDown={(e) => e.key === "Enter" && handleSubmit()}
+              />
+              <Button onClick={handleSubmit} className="mt-2 w-full">
+                Save
+              </Button>
+            </CardContent>
+          </Card>
+        ) : (
+          <Card className="w-[300px] gap-4 md:w-[500px]">
+            <CardHeader>
+              {isChangingUsername ? (
+                <div className="space-y-4">
+                  <CardTitle className="text-center font-normal">
+                    Change Username
+                  </CardTitle>
+                  <div className="space-y-2">
+                    <Input
+                      value={newUsername}
+                      onChange={(e) => setNewUsername(e.target.value)}
+                      placeholder="Enter new username"
+                      onKeyDown={(e) =>
+                        e.key === "Enter" && handleSaveNewUsername()
+                      }
+                    />
+                    <div className="flex gap-2">
+                      <Button
+                        onClick={handleSaveNewUsername}
+                        className="flex-1"
+                      >
+                        Save
+                      </Button>
+                      <Button
+                        variant="outline"
+                        onClick={handleCancelChangeUsername}
+                        className="flex-1"
+                      >
+                        Cancel
+                      </Button>
+                    </div>
                   </div>
                 </div>
-              </div>
-            ) : (
-              <>
-                <CardTitle className="flex items-center justify-center gap-2 text-center font-normal">
-                  Hi, <span className="font-medium">{username}</span>
-                  <Button
-                    variant="ghost"
-                    size="sm"
-                    onClick={handleChangeUsername}
-                    className="hover:text-primary h-6 w-6 cursor-pointer p-0 text-gray-500 hover:bg-transparent"
-                  >
-                    <Edit2 className="h-2 w-2" />
-                  </Button>
-                </CardTitle>
-                <CardDescription className="mt-1 text-center">
-                  Would you like to create or join a playground ?
-                </CardDescription>
-              </>
-            )}
-          </CardHeader>
-          {!isChangingUsername && (
-            <CardContent>
-              <Dialog>
-                <div className="flex items-center justify-center gap-2">
-                  <DialogTrigger asChild>
-                    <Button onClick={() => setDialogType("create")}>
-                      Create
-                    </Button>
-                  </DialogTrigger>
-                  <DialogTrigger asChild>
+              ) : (
+                <>
+                  <CardTitle className="flex items-center justify-center gap-2 text-center font-normal">
+                    Hi, <span className="font-medium">{username}</span>
                     <Button
-                      variant={"outline"}
-                      onClick={() => setDialogType("join")}
+                      variant="ghost"
+                      size="sm"
+                      onClick={handleChangeUsername}
+                      className="hover:text-primary h-6 w-6 cursor-pointer p-0 text-gray-500 hover:bg-transparent"
                     >
-                      Join
+                      <Edit2 className="h-2 w-2" />
                     </Button>
-                  </DialogTrigger>
-                </div>
-                <DialogContent>
-                  <DialogHeader>
-                    <DialogTitle>
-                      {dialogType === "create"
-                        ? "Create Playground"
-                        : "Join Playground"}
-                    </DialogTitle>
-                    <DialogDescription>
-                      {dialogType === "create"
-                        ? "Create a new playground session."
-                        : "Enter a playground code to join an existing session."}
-                    </DialogDescription>
-                  </DialogHeader>
-                  {dialogType === "create" ? (
-                    <CreatePlaygroundForm />
-                  ) : (
-                    <JoinPlaygroundForm />
-                  )}
-                </DialogContent>
-              </Dialog>
-            </CardContent>
-          )}
-        </Card>
-      )}
+                  </CardTitle>
+                  <CardDescription className="mt-1 text-center">
+                    Would you like to create or join a playground ?
+                  </CardDescription>
+                </>
+              )}
+            </CardHeader>
+            {!isChangingUsername && (
+              <CardContent>
+                <Dialog>
+                  <div className="flex items-center justify-center gap-2">
+                    <DialogTrigger asChild>
+                      <Button onClick={() => setDialogType("create")}>
+                        Create
+                      </Button>
+                    </DialogTrigger>
+                    <DialogTrigger asChild>
+                      <Button
+                        variant={"outline"}
+                        onClick={() => setDialogType("join")}
+                      >
+                        Join
+                      </Button>
+                    </DialogTrigger>
+                  </div>
+                  <DialogContent>
+                    <DialogHeader>
+                      <DialogTitle>
+                        {dialogType === "create"
+                          ? "Create Playground"
+                          : "Join Playground"}
+                      </DialogTitle>
+                      <DialogDescription>
+                        {dialogType === "create"
+                          ? "Create a new playground session."
+                          : "Enter a playground code to join an existing session."}
+                      </DialogDescription>
+                    </DialogHeader>
+                    {dialogType === "create" ? (
+                      <CreatePlaygroundForm />
+                    ) : (
+                      <JoinPlaygroundForm />
+                    )}
+                  </DialogContent>
+                </Dialog>
+              </CardContent>
+            )}
+          </Card>
+        )}
+      </div>
     </main>
   );
 }
